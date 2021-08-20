424a7fe4b59a0fce2ce567c8118e356fea11d2d7
Prince Raj <coolfire.prince0@gmail.com>
2021-08-20 08:30:51 +0530

    Clean Simple API created

--- server-system/index.js
+++ server-system/index.js
@@ -1,3 +1,26 @@
-import express from "express"
-import cors from "cors"
-import { Mongoose } from "mongoose"
+const express = require("express");
+const cors = require("cors");
+const mongoose = require('mongoose');
+
+
+//imported modules
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(cors())
+
+
+//connection with database
+mongoose.connect('mongodb://localhost:27017/myMernDB', {useNewUrlParser: true, useUnifiedTopology: true},()=> {console.log("DB connected")});
+
+//Routes
+app.get('/' , (req, res)=>{
+
+   res.send('hello from simple server :)')
+
+})
+
+app.listen(5000,() =>{
+    console.log("DB start at port 5000")
+})
+
