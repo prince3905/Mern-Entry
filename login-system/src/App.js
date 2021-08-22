bed50d9473a44a4ba754b1b751560903c6b90cc0
Prince Raj <coolfire.prince0@gmail.com>
2021-08-22 07:43:42 +0530

    Login and registration Working

--- login-system/src/App.js
+++ login-system/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
      {/* <Homepage/>   */}
-     {/* <Login/> */}
+     <Login/>
      <Register/>
     </div>
   );
--- login-system/src/components/Login/Login.js
+++ login-system/src/components/Login/Login.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 // import PropTypes from 'prop-types';
 import "./Login.css";
-import { Card, FormGroup, FormLabel } from "react-bootstrap";
+import { Card, FormLabel } from "react-bootstrap";
 import { Button } from "react-bootstrap";
 import { Form } from "react-bootstrap";
-import { FloatingLabel } from "react-bootstrap";
 import { Col } from "react-bootstrap";
 import { Row } from "react-bootstrap";
 import { Container } from "react-bootstrap";
+
+import axios from "axios";
 
 function Login() {
   const [user, setUser] = useState({
@@ -24,9 +25,9 @@ function Login() {
     });
   };
 
-  const userInfo = () => {
-    const { email, password } = user;
-    console.log(user);
+  const userLogin = () => {
+    axios.post('http://localhost:5000/login' ,user).then(res => console.log(res)) 
+    // console.log(user);
   };
 
   return (
@@ -44,50 +45,48 @@ function Login() {
         >
           <Row className="justify-content-md-center">
             <Col xs lg="10">
-              
-                <Form.Group className="mb-0" controlId="formBasicEmail">
-                  <FormLabel>
-                    <h1>Login</h1>
-                  </FormLabel>
-                  <Form.Control
-                    type="email"
-                    placeholder="Enter your email"
-                    name="email"
-                    value={user.email}
-                    onChange={handleChange}
-                  />
-                  <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                  </Form.Text>
-                </Form.Group>
+              <Form.Group className="mb-0" controlId="formBasicEmail">
+                <FormLabel>
+                  <h1>Login</h1>
+                </FormLabel>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter your email"
+                  name="email"
+                  value={user.email}
+                  onChange={handleChange}
+                />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
 
-                <Form.Group className="mb-0" controlId="formBasicPassword">
-                  <Form.Control
-                    type="password"
-                    placeholder="Enter your Password"
-                    name="password"
-                    value={user.password}
-                    onChange={handleChange}
-                  />
-                </Form.Group>
+              <Form.Group className="mb-0" controlId="formBasicPassword">
+                <Form.Control
+                  type="password"
+                  placeholder="Enter your Password"
+                  name="password"
+                  value={user.password}
+                  onChange={handleChange}
+                />
+              </Form.Group>
 
-                <div className="d-grid gap-2" style={{ marginTop: "10px" }}>
-                  <Button
-                    variant="primary"
-                    size="sm"
-                    type="submit"
-                    onClick={userInfo}
-                  >
-                    Login
-                  </Button>
+              <div className="d-grid gap-2" style={{ marginTop: "10px" }}>
+                <Button
+                  variant="primary"
+                  size="sm"
+                  type="submit"
+                  onClick={userLogin}
+                >
+                  Login
+                </Button>
 
-                  <h5>Or</h5>
+                <h5>Or</h5>
 
-                  <Button variant="primary" type="submit" size="sm">
-                    Register
-                  </Button>
-                </div>
-          
+                <Button variant="primary" type="submit" size="sm">
+                  Register
+                </Button>
+              </div>
             </Col>
           </Row>
         </Card>
